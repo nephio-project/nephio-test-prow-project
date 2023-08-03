d9d337f184f387ba45ddf357224a78ba121d3c0c
Akash Rajguru <akash.rajguru@est.tech>
2023-08-03 18:15:05 +0100

    commit to test fork repo presubmit jobs

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
+
 import (
-"fmt"
-"nmath/nmath"
+	"fmt"
+	"nmath/nmath"
 )
 
 func main() {
-	a := nmath.Add(1,2)
+	a := nmath.Add(1, 2)
+	fmt.Println("One plus two seems to be:", a)
 	fmt.Println("One plus two seems to be:", a)
 	fmt.Println("One plus two seems to be:", a)
 }
